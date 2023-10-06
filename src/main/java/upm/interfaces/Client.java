e9812bfc591ca739a321fa8c442c86505618dd9c
JBernal <j.bernal@upm.es>
2023-10-06 12:53:22 +0200

    added interfaces

---
+++ src/main/java/upm/interfaces/Client.java
@@ -0,0 +1,16 @@
+package upm.interfaces;
+
+public class Client {
+    private final MyInterface myInterface;
+
+    public Client(MyInterface myInterface) {
+        this.myInterface = myInterface;
+    }
+
+    public void run() {
+        this.myInterface.echo(666);
+        this.myInterface.round(9.9);
+        this.myInterface.defaultMethod();
+        this.myInterface.defaultMethod2(myInterface);
+    }
+}
