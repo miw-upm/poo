24ff7a179e6fafec4165692a45baec2540b74c52
JBernal <j.bernal@upm.es>
2024-10-17 11:47:31 +0200

    added inheritance example

---
+++ src/main/java/upm/inheritances/company/solution/Category.java
@@ -0,0 +1,15 @@
+package upm.inheritances.company.solution;
+
+public enum Category {
+    BASIC(12), SPECIALIST(14);
+
+    private final int base;
+
+    Category(int base) {
+        this.base = base;
+    }
+
+    public int getBase() {
+        return base;
+    }
+}
---
+++ src/main/java/upm/inheritances/company/solution/Secretary.java
@@ -0,0 +1,26 @@
+package upm.inheritances.company.solution;
+
+public class Secretary extends Staff {
+    private final Category category;
+
+    public Secretary(String name, Category category) {
+        super(name);
+        this.category = category;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    @Override
+    public double salary() {
+        return this.category.getBase() * this.getHours();
+    }
+
+    @Override
+    public String toString() {
+        return "Secretary{" +
+                "category=" + category +
+                "} " + super.toString();
+    }
+}
